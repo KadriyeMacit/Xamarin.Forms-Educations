4a49ab254a608a02f83d08c54587d1b59d932d1e
Kadriye <kadriyemacit9655@gmail.com>
2019-08-06 20:40:22 +0300

    Login Page eklendi

---
+++ Login/Login/Login/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Login
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        async void OnClicked(object o, EventArgs args)
+        {
+            await Navigation.PushModalAsync(new Anasayfa());
+
+        }
+
+
+
+    }
+}
