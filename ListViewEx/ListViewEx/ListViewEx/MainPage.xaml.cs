35036f63076974a050af6a320ddb9f2762243be9
Kadriye <kadriyemacit9655@gmail.com>
2019-08-09 20:01:49 +0300

    ListView eklendi.

---
+++ ListViewEx/ListViewEx/ListViewEx/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewEx
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.BindingContext = new[] { "Türkiye", "Portekiz","Polonya","Almanya","İtalya","İspanya" };
+
+        }
+
+      
+
+    }
+}
