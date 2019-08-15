686527d9f4473ba5159ea9e7dbda98d99d6e155c
Kadriye <kadriyemacit9655@gmail.com>
2019-08-16 01:45:36 +0300

    TimePicker örneği eklendi.

---
+++ DateTimeExample/DateTimeExample/DateTimeExample/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DateTimeExample
+{
+    public partial class MainPage : ContentPage
+    {
+
+        DateTime _time;
+
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+
+        void TimePickerPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+
+            if(args.PropertyName == "Time")
+            {
+                _time = DateTime.Today + _timePicker.Time;
+
+                if(_time <DateTime.Now)
+                {
+                    _time += TimeSpan.FromDays(1);
+                }
+
+            }
+
+        }
+
+
+    }
+}
