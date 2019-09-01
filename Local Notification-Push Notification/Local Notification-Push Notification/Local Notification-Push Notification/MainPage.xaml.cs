44b052551bf576a6d1dadb948a539922a3f05822
Kadriye <kadriyemacit9655@gmail.com>
2019-09-01 23:54:26 +0300

    Local Notification/Push Notification eklendi

---
+++ Local Notification-Push Notification/Local Notification-Push Notification/Local Notification-Push Notification/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Plugin.LocalNotifications;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Local_Notification_Push_Notification
+{
+    public partial class MainPage : ContentPage
+    {
+        DateTime _time;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+        }
+
+        bool OnTimerTick()
+        {
+            if (_switch.IsToggled && DateTime.Now >= _time)
+            {
+                _switch.IsToggled = false;
+
+                CrossLocalNotifications.Current.Show(_entry.Text, _editor.Text);
+
+
+            }
+
+            return true;
+        }
+
+        void TimePickerPropertyChanged(object sender, PropertyChangedEventArgs args)
+        {
+
+            if (args.PropertyName == "Time")
+            {
+                SetTriggerTime();
+
+            }
+
+        }
+
+
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            SetTriggerTime();
+        }
+
+
+        void SetTriggerTime()
+        {
+            _time = DateTime.Today + _timePicker.Time;
+
+            if (_time < DateTime.Now)
+            {
+                _time += TimeSpan.FromDays(1);
+            }
+        }
+    }
+}
